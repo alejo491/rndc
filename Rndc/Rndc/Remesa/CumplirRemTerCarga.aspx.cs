0cf3298d0ef26ec5fa797694d709cac005b37567
juandavidpinto4444 <juandavidpinto4444@9f683288-16a4-b42f-f4e3-84571cf99939>
2014-06-03 23:56:46 +0000

    actualizacion de archivo cumplirremtercarga con la funcion btn_guardar_cumplido.

--- Rndc/Rndc/Remesa/CumplirRemTerCarga.aspx.cs
+++ Rndc/Rndc/Remesa/CumplirRemTerCarga.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Rndc.Servicios;
 
 namespace Rndc.Remesa
 {
@@ -12,6 +13,11 @@ namespace Rndc.Remesa
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btn_guardar_cumplido_remesa_Click(object sender, EventArgs e)
+        {
+            ServicioWebRndc.CrearCumplirRemesa((this.txt_usuario.Text).ToString(),"",(this.txt_nit.Text).ToString(),(this.txt_consecutivo.Text).ToString(),(this.txt_manifiesto.Text).ToString(),(this.slt_tipo_cumplido.SelectedValue).ToString(),(this.txt_fecha_llegada_destino.Text).ToString(),(this.txt_hora_llegada_destino.Text).ToString(),(this.txt_fecha_entrada_destino.Text).ToString(),(this.txt_hora_entrada_destino.Text).ToString(),(this.txt_fecha_salida_destino.Text).ToString(),(this.txt_hora_salida_destino.Text).ToString(),(this.txt_cantidad_entregada.Text).ToString());
         }
     }
 }
