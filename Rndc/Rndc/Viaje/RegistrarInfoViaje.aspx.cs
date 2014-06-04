2faa3be8c20c7fb44c950f791588f88cb89fdcb7
jhonyanguas@gmail.com <jhonyanguas@gmail.com@9f683288-16a4-b42f-f4e3-84571cf99939>
2014-06-04 21:01:05 +0000

    validaciones viaje

--- Rndc/Rndc/Viaje/RegistrarInfoViaje.aspx.cs
+++ Rndc/Rndc/Viaje/RegistrarInfoViaje.aspx.cs
@@ -17,8 +17,21 @@ namespace Rndc.Viaje
 
         protected void btn_registrar_info_viaje_Click(object sender, EventArgs e)
         {
-             ServicioWebRndc.CreacionInformacionViaje((this.txt_usuario.Text).ToString(), "", (this.txt_empresa.Text).ToString(), (this.txt_consecutivo.Text).ToString(),"codIDConductor",(this.txt_num_identi_cond.Text).ToString(), (this.txt_placa1.Text).ToString(),(this.txt_placa2.Text).ToString(),(this.txt_origen.Text).ToString(),(this.txt_destino.Text).ToString());
-            //revisar cual es el codid del conductor
+            bool validar = true;
+            string mensaje = "";
+
+            if (int.Parse(this.txt_valor_viaje.Text) == 0)
+            {
+                mensaje = mensaje + Environment.NewLine + "El valor debe ser un valor numerico mayor o igual a 0";
+                validar = false;
+            
+            }
+
+            if (validar)
+            {
+                ServicioWebRndc.CreacionInformacionViaje((this.txt_usuario.Text).ToString(), "", (this.txt_empresa.Text).ToString(), (this.txt_consecutivo.Text).ToString(), "codIDConductor", (this.txt_num_identi_cond.Text).ToString(), (this.txt_placa1.Text).ToString(), (this.txt_placa2.Text).ToString(), (this.txt_origen.Text).ToString(), (this.txt_destino.Text).ToString());
+                //revisar cual es el codid del conductor
+            }
         }
 
     }
