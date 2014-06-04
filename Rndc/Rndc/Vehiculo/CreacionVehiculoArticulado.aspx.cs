0bf9f50328c447c1eb508ee54b44cdd404a337cc
jhonyanguas <jhonyanguas@9f683288-16a4-b42f-f4e3-84571cf99939>
2014-06-04 23:12:46 +0000

    validaciones vehiculo.

--- Rndc/Rndc/Vehiculo/CreacionVehiculoArticulado.aspx.cs
+++ Rndc/Rndc/Vehiculo/CreacionVehiculoArticulado.aspx.cs
@@ -37,22 +37,45 @@ namespace Rndc.Vehiculo
 
         protected void btnGuardar_OnClick(object sender, EventArgs e)
         {
-            
-            
-            try
+            bool validar = true;
+            string mensaje = "";
+            double peso;
+            if (int.Parse(this.txtModelo.Text) < 1900 || int.Parse(this.txtModelo.Text)<new DateTime().Year+1)
             {
-                string respuesta = ServicioWebRndc.CreacionVehiculoArticulado(this.txtUsuario.Text, "", this.txtNit.Text, this.txtPlaca.Text, this.sltConfiguracion.SelectedValue, this.marca.SelectedValue, this.txtModelo.Text, this.txtPeso.Text, this.txt_capacidad_uni_carga.Text, this.color.SelectedValue, this.carroceria.SelectedValue, this.tipoIdPropietario.SelectedValue, this.txt_num_ident_propi.Text, this.tipoIdtenedor.SelectedValue, this.txtNumIdTenedor.Text);
-                ServicioDbRndc.CreacionVehiculoArticulado(this.txtPlaca.Text, this.carroceria.SelectedValue, this.color.SelectedValue, this.marca.SelectedValue, this.sltConfiguracion.SelectedValue, this.txtModelo.Text, this.txtPeso.Text);
-                
-                ClientScript.RegisterStartupScript(this.GetType(), "myScript",
-                                "<script>alert('" + ManejadorErrores.Solucion(respuesta, "Crear Vehiculo Articulado") + "')</script>");
+                validar = false;
+                mensaje = mensaje + Environment.NewLine + "El modelo del vehiculo no puede ser menor a 1900 o mayor a " + (new DateTime().Year + 1).ToString();
             }
-            catch (Exception)
+            if (double.TryParse(this.txtPeso.Text, out peso) || this.txtPeso.Text.Length < 3 || this.txtPeso.Text.Length > 5 || peso<200 || peso >53000)
             {
+                validar = false;
+                mensaje = mensaje + Environment.NewLine + "El peso debe registrarse en kilogramos,debe ser de 3 a 5 dígitos, y debe ser mayor a 200 kilogramos y menor a 53000 kilogramos ";
 
-                ClientScript.RegisterStartupScript(this.GetType(), "myScript",
-                            "<script>alert('Error en la persistencia de la aplicacion,')</script>");
             }
+            if(this.txt_num_ident_propi.Text.Length <10)
+            {
+                validar = false;
+                mensaje = mensaje + Environment.NewLine + "el numero de identificacion debe ser máximo de 10 caracteres";
+            }
+            
+            if(validar)
+            {
+                  try
+                  {
+                    string respuesta = ServicioWebRndc.CreacionVehiculoArticulado(this.txtUsuario.Text, "", this.txtNit.Text, this.txtPlaca.Text, this.sltConfiguracion.SelectedValue, this.marca.SelectedValue, this.txtModelo.Text, this.txtPeso.Text, this.txt_capacidad_uni_carga.Text, this.color.SelectedValue, this.carroceria.SelectedValue, this.tipoIdPropietario.SelectedValue, this.txt_num_ident_propi.Text, this.tipoIdtenedor.SelectedValue, this.txtNumIdTenedor.Text);
+                    ServicioDbRndc.CreacionVehiculoArticulado(this.txtPlaca.Text, this.carroceria.SelectedValue, this.color.SelectedValue, this.marca.SelectedValue, this.sltConfiguracion.SelectedValue, this.txtModelo.Text, this.txtPeso.Text);
+                
+                    ClientScript.RegisterStartupScript(this.GetType(), "myScript",
+                                "<script>alert('" + ManejadorErrores.Solucion(respuesta, "Crear Vehiculo Articulado") + "')</script>");
+                    }
+                    catch (Exception)
+                    {
+
+                        ClientScript.RegisterStartupScript(this.GetType(), "myScript",
+                                "<script>alert('Error en la persistencia de la aplicacion,')</script>");
+                    }
+            }
+            
+          
         }
 
         protected void txt_num_ident_propi_OnTextChanged(object sender, EventArgs e)
