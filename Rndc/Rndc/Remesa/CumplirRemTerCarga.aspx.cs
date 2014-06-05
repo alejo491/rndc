471ee2798381fd67dfebe6a504e56dc3d4827216
jhonyanguas@gmail.com <jhonyanguas@gmail.com@9f683288-16a4-b42f-f4e3-84571cf99939>
2014-06-05 02:08:28 +0000

    validaciones de remesa terrerstre carga y registrar info viaje de la carpeta viaje

--- Rndc/Rndc/Remesa/CumplirRemTerCarga.aspx.cs
+++ Rndc/Rndc/Remesa/CumplirRemTerCarga.aspx.cs
@@ -17,7 +17,20 @@ namespace Rndc.Remesa
 
         protected void btn_guardar_cumplido_remesa_Click(object sender, EventArgs e)
         {
-            ServicioWebRndc.CrearCumplirRemesa((this.txt_usuario.Text).ToString(),"",(this.txt_nit.Text).ToString(),(this.txt_consecutivo.Text).ToString(),(this.txt_manifiesto.Text).ToString(),(this.slt_tipo_cumplido.SelectedValue).ToString(),(this.txt_fecha_llegada_destino.Text).ToString(),(this.txt_hora_llegada_destino.Text).ToString(),(this.txt_fecha_entrada_destino.Text).ToString(),(this.txt_hora_entrada_destino.Text).ToString(),(this.txt_fecha_salida_destino.Text).ToString(),(this.txt_hora_salida_destino.Text).ToString(),(this.txt_cantidad_entregada.Text).ToString());
+
+            bool validar = true;
+            string mensaje = "";
+            if(int.Parse(this.txt_cantidad_cargada.Text) > (int.Parse(this.txt_cantidad_entregada.Text)))
+            {
+                mensaje = mensaje + Environment.NewLine + "La cantidad entregada debe ser menor o igual a la cantidad cargada";
+                validar = false;
+            }
+
+
+            if (validar)
+            {
+                ServicioWebRndc.CrearCumplirRemesa((this.txt_usuario.Text).ToString(), "", (this.txt_nit.Text).ToString(), (this.txt_consecutivo.Text).ToString(), (this.txt_manifiesto.Text).ToString(), (this.slt_tipo_cumplido.SelectedValue).ToString(), (this.txt_fecha_llegada_destino.Text).ToString(), (this.txt_hora_llegada_destino.Text).ToString(), (this.txt_fecha_entrada_destino.Text).ToString(), (this.txt_hora_entrada_destino.Text).ToString(), (this.txt_fecha_salida_destino.Text).ToString(), (this.txt_hora_salida_destino.Text).ToString(), (this.txt_cantidad_entregada.Text).ToString());
+            }
         }
     }
 }
